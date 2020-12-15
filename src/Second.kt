c195586fd849f8a9c359ef319d454a41bc320b0e
anubhavbagri <anubhav.199302029@muj.manipal.edu>
2020-12-15 15:53:10 +0530

    Updated second.kt  - For-Each loop in kotlin  - Other loops syntax

--- src/Second.kt
+++ src/Second.kt
@@ -1,3 +1,4 @@
+// ; means end of the line but its redundant unless writing multiple statements on the same line.
 fun main(){
 //    it works on vararg
     /*
@@ -5,7 +6,7 @@ fun main(){
      */
     val numbers = arrayOf(1,2,3,4,5)
     val name = arrayOf("1","2","3","4",5, 6.34) //we haven't given any type of array
-    val nameString = arrayOf<String>("a","b","c","d","e")
+    val nameString = arrayOf<String>("anubhav","ayush","suhit","devraj","rishav")
     nameString.set(0,"z")
     println(nameString[0])
     print(numbers[0])
@@ -17,17 +18,78 @@ fun main(){
     /*
     lists or Collections
      */
-    //immutable lists
+    /*
+        to make immutable lists
+     */
     var listOfNames = listOf<String>("Hello","world","coder","kotlin")
     var mapOfNames = mapOf<Int, String>(0 to "Hello",3 to "world",2 to "coder",7 to "kotlin")
     println(mapOfNames[7])
     var mapOfNames1 = mapOf("name" to "Hello",3.87 to "world",2 to 20,9 to "kotlin")
-    var hashMapOfNames = hashMapOf<int,int>(0 to 3, 2 to 7, 4 to 97)
-    var hashMapOfNames1 = hashMapOf("name" to "Hello",3.87 to "world",2 to 20,9 to "scene")
-//    hashMapOfNames.add()
-    
-    //to mutate a list
+    println(mapOfNames1["name"])
+    //    var hashMapOfNames = hashMapOf<int,int>(0 to 3, 2 to 7, 4 to 97)
+    /*
+        to make mutable lists
+     */
     var mutableListOfNames = mutableListOf<String>("Hi","anubhav","bagri")
     mutableListOfNames.add("Pulkit")
     var mutableArrayListOfNames = arrayListOf<String>("don't know","Yes","No")
+    var hashMapOfNames = hashMapOf("name" to "Hello",3.87 to "world",2 to 20,9 to "scene")
+    //    hashMapOfNames.add("agarwal")
+    hashMapOfNames.set("work","google")
+    //    hashMapOfNames["work"] = "google"
+    println(hashMapOfNames["work"])
+    /*
+        For-each loop : iterate over the entire set of items/collection of items and gives the value of every item
+     */
+    hashMapOfNames.forEach { (any, any2) ->
+        println("" + any + any2)
+    }
+    mutableArrayListOfNames.forEach {
+        println(it)
+    }
+    nameString.forEach {
+        println(it)
+    }
+    numArray.forEach {
+        println(it)
+    }
+    println("----")
+    /*
+    other Loops in kotlin
+     */
+    for(name in nameString){
+        println(name)
+    }
+    println("----")
+    for(x in 0..10){
+        //10 is inclusive
+        println(x)
+    }
+    println("----")
+    for(x in 0 until 10){
+        //10 is exclusive
+        println(x)
+    }
+    println("----")
+    for(x in 0 until 10 step 2){
+        //10 is exclusive, prints forward 0 2 4 6 8
+        println(x)
+    }
+    println("----")
+    for(x in 10 downTo 0 step 2){
+        //0 is inclusive, prints backward 10 8 6 4 2 0
+        println(x)
+    }
+    println("----")
+    var a = 10
+    while (a>0){
+        print(" $a")
+        a-=3
+    }
+    println()
+    do {
+        print(" $a")
+        a-=2
+    }while (a>-10);
 }
+
