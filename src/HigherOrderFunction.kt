4351ed6764941d2bd85dedbfa9264a710bfb1bbb
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-01-25 17:57:57 +0530

    Update HigherOrderFunction.kt

--- src/HigherOrderFunction.kt
+++ src/HigherOrderFunction.kt
@@ -3,9 +3,10 @@ fun main(args: Array<String>) {
     var y : Int = x.toInt()
     y++
     println("Hello World" + y)
+    val osmString = awesomeFun("Ayush") //val osmString : String = awesomeFun("Ayush")
+    println(osmString)
 
     getUser("Bagri"){
-
     }
 }
 
@@ -23,4 +24,11 @@ fun getUser(name: String, doNothing:(IsActive: Boolean) -> Unit): String{
     }
 
     return "Anubhav $name"
+}
+
+//By default, functions return a datatype called 'Unit'
+//Unit means a function does not return anything
+
+fun awesomeFun(name: String): String {
+    return "$name is awesome!"
 }
