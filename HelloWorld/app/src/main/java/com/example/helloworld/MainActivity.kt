bf40608a484cc053464e632d5babc25aca8b9d6b
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-27 02:30:04 +0530

    feat: add logic to show text

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var personName : EditText
+    lateinit var viewName : Button
+    lateinit var showName : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewName = findViewById(R.id.viewName)  //find the view in the layout with the id
     }
+
+    fun showName(view: View){
+        personName = findViewById(R.id.personName)
+        showName = findViewById(R.id.showName)
+
+        var name = personName.text
+        showName.setText(name)
+
+     }
+
 }
